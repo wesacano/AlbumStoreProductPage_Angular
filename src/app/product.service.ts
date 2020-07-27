7e4f0fa38e8895a7622776211a0955e188dce27c
Wes Cano <wesacano@gmail.com>
2020-07-26 19:37:03 -0500

    Imported Album interface into product.service and product-description.html

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map'
-
+import { Album } from './album';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
